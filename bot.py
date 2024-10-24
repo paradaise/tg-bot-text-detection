f204db19066c00bca32ab511406e558a0f8e2c89
paradaise <paradaiseru@gmail.com>
2024-10-24 09:40:33 +0300

    git commit --amend
    change

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from conf import TOKEN
 bot = telebot.TeleBot(TOKEN)
 
 main_menu = ('📑Контакты','🔗Другое','💸Поддержать')
-donation_menu = ('🫰Юмани','💰СБП','↩️Назад')
+donation_menu = ('🫰Юмани','СБП','↩️Назад')
 
 def keyboard(menu):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -18,19 +18,23 @@ def keyboard(menu):
 
 @bot.message_handler(commands=['start'])
 def start(message):
+
     username = message.from_user.username
     name = message.from_user.first_name
     bot.reply_to(message, "📸Привет,<b>{name}, AKA {username}</b>,отправь мне фотографию и посмотри что получится!",reply_markup = keyboard(main_menu),parse_mode = "html")
 
+    bot.reply_to(message, "Привет,я ищу текст на рекламных плоскостях!Отправь фото и проверь!📸",reply_markup = keyboard(main_menu))
+
+
 @bot.message_handler(content_types=['audio', 'video', 'document', 'location', 'contact', 'sticker'])
 def handle_unsupported(message):
-    bot.reply_to(message, "Извините, пока я работаю только с изображениями🙁")
+    bot.reply_to(message, "Извините, пока я работаю только с изображениями🙁,скоро будет обработка видео контента!")
 
 @bot.message_handler(content_types=['text'])
 def get_information(message):
     if message.chat.type == 'private':
         if message.text == '📑Контакты':
-            bot.send_message(message.chat.id,'У вас что-то не работает?Или есть предложения о сотрудничестве?Напишите нам!\nГуреев Кирилл:\n📱Telegram: t.me/Valer04ka1488\n🌐Вконтакте: vk.com/abchik1488\n🐙GitHub:\n')
+            bot.send_message(message.chat.id,'У вас что-то не работает?Или есть предложения о сотрудничестве?Напишите нам!\nГуреев Кирилл:\n📱Telegram: t.me/Valer04ka1488\n🌐Вконтакте: vk.com/abchik1488\n🐙GitHub:github.com/Abchik\n')
             bot.send_message(message.chat.id,'Гоголев Виктор:\n📱Telegram: t.me/wa55up\n🌐Вконтакте: vk.com/yowa55up\n🐙GitHub: github.com/paradaise\n')
         elif message.text == '🔗Другое':
             bot.send_message(message.chat.id,'🚫В разработке,пока недоступно🚫')
@@ -38,8 +42,6 @@ def get_information(message):
             bot.send_message(message.chat.id,'💵Вы можете поддержать наш проект,нажав кнопку ниже:', reply_markup = keyboard(donation_menu))
         elif message.text == '🫰Юмани':
             bot.send_message(message.chat.id,'🫰Вы можете поддержать Юмани по ссылке:\nhttps://yoomoney.ru/to/410013032669115')
-        elif message.text == '💰СБП':
-            bot.send_message(message.chat.id,'💰Вы можете поддержать CБП по ссылке:')
         elif message.text == '↩️Назад':
             bot.send_message(message.chat.id,'↩️Возвращаемся к основному меню', reply_markup = keyboard(main_menu))
 
@@ -49,11 +51,12 @@ def handle_photo(message):
     with open(img_path, 'wb') as new_file:
         new_file.write(bot.download_file(bot.get_file(message.photo[-1].file_id).file_path))
 
-        result_msg, result_img = crop_and_ocr(img_path)
+        result_msg, result_imgs = crop_and_ocr(img_path)
 
-        bot.reply_to(message, result_msg)
-        if result_img is not None:
-            bot.send_photo(message.chat.id, result_img)
+        bot.reply_to(message, result_msg,parse_mode="html")
+        if result_imgs is not None:
+            for img in result_imgs:
+                bot.send_photo(message.chat.id, img)
 
 # Запуск бота
 bot.polling(none_stop=True)
